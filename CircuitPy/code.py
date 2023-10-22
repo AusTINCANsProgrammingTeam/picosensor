70780b3a53ff4228b879aa1db7afd8471e1b17c7
jcassar <jp@cassartx.net>
2023-10-22 17:02:06 -0500

    Added VL6180X support
    Added VL6180X support and code refactory.

--- CircuitPy/code.py
+++ CircuitPy/code.py
@@ -1,21 +1,40 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-FileCopyrightText: 2024 the ausTIN CANs FRC#2158
 # SPDX-License-Identifier: MIT
 
-# Simple demo of the VL53L0X distance sensor.
-# Will print the sensed range/distance every second.
+# Read VL53L0X and VL6180X distance sensors.
+# Print the sensed range/distance and light Lux every second.
 import time
 
 import busio
 import board
+import digitalio
 
 import adafruit_vl53l0x
+import adafruit_vl6180x
+
+# Raspberry Pi Pico onboard LED
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
 
 # Initialize I2C bus and sensor.
-i2c1 = busio.I2C(board.GP5, board.GP4)
-vl53_1 = adafruit_vl53l0x.VL53L0X(i2c1)
+# VL53L0X - default I2C address is 0x29 and changed to 0x30
+# 50 - 1200 mm of range distance
+#
+# instantiate the VL53L0X sensor on the I2C bus
+i2c1 = busio.I2C(board.SCL, board.SDA)
+vl53l0 = adafruit_vl53l0x.VL53L0X(i2c1)
+vl53l0.set_address(0x30)   # address assigned should NOT be already in use
 
-i2c2 = busio.I2C(board.GP7, board.GP6)
-vl53_2 = adafruit_vl53l0x.VL53L0X(i2c2)
+# VL6180X - I2C address is 0x29 and cannot be changed
+# 5 - 200 mm of range distance
+#
+# instantiate the VL6180X sensor on the I2C bus
+i2c2 = busio.I2C(board.SCL, board.SDA)
+vl6180 = adafruit_vl6180x.VL6180X(i2c2)
+
+# Optionally add an offset to distance measurements here (e.g. calibration)
+# Swapping for the following would add a +10 millimeter offset to measurements:
+# vl6180 = adafruit_vl6180x.VL6180X(i2c2, offset=10)
 
 # Optionally adjust the measurement timing budget to change speed and accuracy.
 # See the example here for more details:
@@ -26,7 +45,10 @@ vl53_2 = adafruit_vl53l0x.VL53L0X(i2c2)
 # vl53.measurement_timing_budget = 200000
 # The default timing budget is 33ms, a good compromise of speed and accuracy.
 
-# Main loop will read the range and print it every second.
+# Main loop will read the ranges (mm) and light (lux) and print them every second.
 while True:
-    print("{0}, {0}".format(vl53_1.range, vl53_2.range))
-    time.sleep(1.0)
+    led.value = True
+    light_lux = vl6180.read_lux(adafruit_vl6180x.ALS_GAIN_1)
+    print("{0}, {0}, {0}".format(vl53l0.range, vl6180.range, light_lux))
+    led.value = False
+    time.sleep(1.0)  # Sleep for one second.
