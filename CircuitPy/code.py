462e56372e9a0b27039cd66a806b11d6540df2b6
jcassar <jp@cassartx.net>
2023-10-31 08:35:27 -0500

    Multiplexing and data updates.
    Using a PCA9546A to perform I2C addresses multiplexing between the VL53L0X and VL6180 sensors. Use only the VL53L0X range data when distance is greater than 254mm.

--- CircuitPy/code.py
+++ CircuitPy/code.py
@@ -5,50 +5,60 @@
 # Print the sensed range/distance and light Lux every second.
 import time
 
-import busio
 import board
+import busio
+
 import digitalio
 
 import adafruit_vl53l0x
 import adafruit_vl6180x
+import adafruit_tca9548a
 
 # Raspberry Pi Pico onboard LED
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 # Initialize I2C bus and sensor.
-# VL53L0X - default I2C address is 0x29 and changed to 0x30
-# 50 - 1200 mm of range distance
-#
-# instantiate the VL53L0X sensor on the I2C bus
-i2c1 = busio.I2C(board.SCL, board.SDA)
-vl53l0 = adafruit_vl53l0x.VL53L0X(i2c1)
-vl53l0.set_address(0x30)   # address assigned should NOT be already in use
+# Adafruit
+i2c0 = busio.I2C(board.GP5, board.GP4)
 
-# VL6180X - I2C address is 0x29 and cannot be changed
-# 5 - 200 mm of range distance
-#
-# instantiate the VL6180X sensor on the I2C bus
-i2c2 = busio.I2C(board.SCL, board.SDA)
-vl6180 = adafruit_vl6180x.VL6180X(i2c2)
+# Create the PCA9546A object and give it the I2C bus
+try:
+    # Port 0 is connected to VL6180
+    # Port 1 is connected to VL53L0
+    # Port 2 and 3 are free for others sensors
+    mux = adafruit_tca9548a.PCA9546A(i2c0)
+    # VL6180X - I2C address is 0x29 and cannot be changed
+    # 5 - 200 mm of range distance
+    # instantiate the VL6180X sensor using the PCA9546A channel instead
+    # of the I2C object
+    vl6180 = adafruit_vl6180x.VL6180X(mux[0])
 
-# Optionally add an offset to distance measurements here (e.g. calibration)
-# Swapping for the following would add a +10 millimeter offset to measurements:
-# vl6180 = adafruit_vl6180x.VL6180X(i2c2, offset=10)
+    # VL53L0X - default I2C address is 0x29
+    # 50 - 1200 mm of range distance
+    #
+    # instantiate the VL53L0X sensor using the PCA9546A channel instead
+    # of the I2C object
+    vl53l0 = adafruit_vl53l0x.VL53L0X(mux[1])
 
-# Optionally adjust the measurement timing budget to change speed and accuracy.
-# See the example here for more details:
-#   https://github.com/pololu/vl53l0x-arduino/blob/master/examples/Single/Single.ino
-# For example a higher speed but less accurate timing budget of 20ms:
-# vl53.measurement_timing_budget = 20000
-# Or a slower but more accurate timing budget of 200ms:
-# vl53.measurement_timing_budget = 200000
-# The default timing budget is 33ms, a good compromise of speed and accuracy.
+    # Main loop will read the ranges (mm) and light (lux) and print them every second.
+    while True:
+        led.value = True
+        light_lux = vl6180.read_lux(adafruit_vl6180x.ALS_GAIN_1)
+        # Range are millimeters, illuminance in Lux 
+        rangeVL6180 = vl6180.range
+        rangeVL53L0 = vl53l0.range
+        # VL6180 is more precise, so use it for less than 255
+        if rangeVL6180 >= 255:
+            range = rangeVL53L0
+        else:
+            range = rangeVL6180
+        print(range, light_lux)
+        led.value = False
+        time.sleep(1.0)  # Sleep for one second.
+except (OSError) as e:
+    print("{}. PCA9546A Not found at address 0x70".format(e))
+except (ValueError) as ve:
+    print("{}.".format(ve))
 
-# Main loop will read the ranges (mm) and light (lux) and print them every second.
-while True:
-    led.value = True
-    light_lux = vl6180.read_lux(adafruit_vl6180x.ALS_GAIN_1)
-    print("{0}, {0}, {0}".format(vl53l0.range, vl6180.range, light_lux))
-    led.value = False
-    time.sleep(1.0)  # Sleep for one second.
+
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # picosensor
+# 2023 the ausTIN CANs FRC#2158
+# License: MIT
 CircuitPython to read distance sensor data on a Raspberry Pi Pico in order to send the data to a roboRIO.
 
-Use Adafruit VL53L0X and VL6180X distance sensors on I2C bus (STEMMA/QT connections)
+Use Adafruit VL53L0X and VL6180X distance sensors on I2C bus (STEMMA/QT connections). I2C addresses multiplexing is performed by
+an Adafruit PCA9546A board.
 
 The roboRIO can provide Ground, 5v, and the communication signal to the device.
-
-5v on the roboRIO is MXP pin 1 (Top right pin) and connects to pin 39 (VSYS) of the Pi Pico.
-GND on the roboRIO is pin 8 (bottom row, 4th in from the right) and connects to either Pin 3 or Pin 38 (GND) of the Pi Pico.
 UART Rx on the roboRIO is pin 10 (bottom row, 5th in from the right) and connects to pin 1 (UART0 TX) of the Pi Pico.
 
-Adafruit PiCowbell Proto for Pico is used to provide the STEMMA QT / Qwiic connector for fast I2C
+Adafruit PiCowbell Proto for Pico is used to provide the STEMMA QT / Qwiic connector for fast I2C, using GP4 and GP5
